68964560ab96a8908bae8d648c35ffcdd18d64c2
Jacob Folch <jsf.devops@gmail.com>
2019-04-05 16:30:58 +0200

    pene

--- src/clases/ControladorCoche.java
+++ src/clases/ControladorCoche.java
@@ -1,5 +1,29 @@
 package clases;
 
 public class ControladorCoche {
+	
+	private Interfaz GUI;
+	private Pedales targetInput;
+	private Salpicadero targetDisplay;
+	
+	private GestorFiltros gFiltros;
+	
+	private EstadoMotor estado;
+	
+	public ControladorCoche() {
+		
+		// Estado Inicial
+		this.estado = EstadoMotor.APAGADO;
+		
+		// Interaccion con el humano
+		this.GUI = new Interfaz();
+		this.targetInput = this.GUI.getPedales();
+		this.targetDisplay = this.GUI.getSalpicadero();
+		
+		// Gestor de filtros
+		this.gFiltros = new GestorFiltros();
+		
+		
+	}
 
 }
