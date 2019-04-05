4c8efc5357dd2c4065a882a0fd8cb166260c0c06
Jacob Folch <jsf.devops@gmail.com>
2019-04-05 17:04:02 +0200

    hola

--- src/clases/ControladorCoche.java
+++ src/clases/ControladorCoche.java
@@ -1,16 +1,18 @@
 package clases;
 
-public class ControladorCoche {
+public class ControladorCoche extends Thread {
 	
 	private Interfaz GUI;
 	private Pedales targetInput;
 	private Salpicadero targetDisplay;
 	
+	private Motor motor;
+	
 	private GestorFiltros gFiltros;
 	
 	private EstadoMotor estado;
 	
-	public ControladorCoche() {
+	public ControladorCoche(Motor m) {
 		
 		// Estado Inicial
 		this.estado = EstadoMotor.APAGADO;
@@ -20,10 +22,31 @@ public class ControladorCoche {
 		this.targetInput = this.GUI.getPedales();
 		this.targetDisplay = this.GUI.getSalpicadero();
 		
+		// Motor
+		this.motor = new Motor(this.estado);
+		
 		// Gestor de filtros
-		this.gFiltros = new GestorFiltros();
+		this.gFiltros = new GestorFiltros(this.motor);		
+	}
+	
+	@Override
+	public void run() {
+		// Lee de los controles
+		this.estado = this.targetInput.getEstado();
 		
+		// Lee del Motor
+		double rpm = motor.getRevoluiones();
 		
+		// Invoca jestor de filtros
+		this.gFiltros.ejecutar(rpm, this.estado);
+		
+		// Actualiza Display
+		this.targetDisplay.actualizaInformacion(this.motor);
+	}
+	
+	public void encender() {
+		this.GUI.setVisible(true);
+		this.start();
 	}
 
 }
